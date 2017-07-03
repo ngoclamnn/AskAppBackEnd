a4b639f4fa403f61397fca3bc480c8b4866e05a2
ORIENTSOFTWARE\nnlam <lam.nguyen@orientsoftware.net>
2017-07-03 16:40:59 +0700

    more code

---
+++ AskAppBackEnd.WebServiceAPI/Controllers/ODataControllerBase.cs
@@ -0,0 +1,69 @@
+﻿using AskAppBackEnd.Core;
+using System;
+using System.Data.Entity;
+using System.Web.Http;
+using System.Web.OData;
+
+namespace AskAppBackEnd.Controllers
+{
+    public class ODataControllerBase<T> : ODataController where T : EntityBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDbSet<T> _dbSet;
+        public ODataControllerBase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _dbSet = _unitOfWork.Set<T>();
+        }
+        public IHttpActionResult Post([FromBody] T entity)
+        {
+            try
+            {
+                _dbSet.Add(entity);
+                _unitOfWork.Save();
+                return Ok(entity);
+            }
+            catch (ArgumentNullException e)
+            {
+                return BadRequest();
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest();
+            }
+            catch (InvalidOperationException e)
+            {
+                return Conflict();
+            }
+        }
+
+        public IHttpActionResult Put(Guid key, T entity)
+        {
+            try
+            {
+                entity.Id
+                _unitOfWork.DbContext.Entry(entity).State = EntityState.Modified;
+                _unitOfWork.Save();
+                //Mapping here
+                return Ok(entity);
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest();
+            }
+            catch (ArgumentException)
+            {
+                return NotFound();
+            }
+        }
+
+        public IHttpActionResult Delete(int key)
+        {
+            var entityRemove = _dbSet.Find(key);
+            if (entityRemove != null)
+                return Ok(_dbSet.Remove(entityRemove));
+            else
+                return NotFound();
+        }
+    }
+}
