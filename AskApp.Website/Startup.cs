57cf48603cb5c233898e58ac118be28ac44a7645
Nguyễn Ngọc Lam <ngoclamn@live.com>
2017-08-27 22:57:47 +0700

    Add website project

---
+++ AskApp.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AskApp.Website.Startup))]
+namespace AskApp.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
